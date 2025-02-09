fd1d3ae795131a54210e6f20a9484d465cf9b6a3
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-02-09 15:21:44 +0530

    Add files via upload

---
+++ 04_comparision.js
@@ -0,0 +1,23 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+/*console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0); //<,> in the js have different approach 
+console.log(null == 0); //== and === in the js have different approach
+console.log(null >= 0); // also >= in the js have a different approach
+*/
+console.log(undefined == 0); //
+console.log(undefined < 0);
+console.log(undefined > 0);
+ 
+// === 
+
+console.log("2" == 2); // loose equality
+console.log("2" === 2);// strict check 
+
+

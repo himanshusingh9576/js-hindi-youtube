f4bce30f647d7253784517e91fc68ee4dd92a531
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-02-09 10:42:42 +0000

    done

--- 04_comparision.js
+++ 04_comparision.js
@@ -4,16 +4,18 @@
 //console.log(2 == 1);
 //console.log(2 != 1);
 
-/*console.log("2" > 1);
-console.log("02" > 1);
+//console.log("2" > 1);
+//console.log("02" > 1);
 
-console.log(null > 0); //<,> in the js have different approach 
-console.log(null == 0); //== and === in the js have different approach
-console.log(null >= 0); // also >= in the js have a different approach
-*/
+//console.log(null > 0); //<,> in the js have different approach 
+//console.log(null == 0); //== and === in the js have different approach
+//console.log(null >= 0); // also >= in the js have a different approach
+
 console.log(undefined == 0); //
 console.log(undefined < 0);
-console.log(undefined > 0);
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
  
 // === 
 
